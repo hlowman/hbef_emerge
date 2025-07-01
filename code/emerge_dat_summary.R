45fb544727c992bfccb2f78e86aeb9ff4da44772
Heili Lowman <heili.lowman@gmail.com>
2025-07-01 14:11:49 -0400

    Made additional figures to bring to HB next week.

--- code/emerge_dat_summary.R
+++ code/emerge_dat_summary.R
@@ -11,7 +11,7 @@
 # in real time and cumulative plots by watershed.
 
 # Data was downloaded from https://hbwater.org/restricted_QAQC/
-# on May 13, 2025.
+# on July 1, 2025.
 
 #### Setup ####
 
@@ -23,16 +23,17 @@ library(calecopal)
 library(viridis)
 
 # Load data.
-dat <- read_csv("data_raw/sticky_trap_counts_051325.csv")
+dat <- read_csv("data_raw/sticky_trap_counts_070125.csv")
 
 # Hex values for later use
 "#3900b3ff", "#714dbfff", "#9e6b90ff", "#cf9270ff", "#ebb698ff"
 "#3900B3", "#5524BD", "#7148C8", "#8D6DD3", "#AA91DE", "#C6B6E9"
 "#404873ff", "#505990ff", "#5f6aadff", "#6f7bcaff", "#7e8be6ff", "#bfc5f3ff"
+"#D3E3CA" "#C9DDBF" "#C0D7B5" "#AFC5A4" "#9BAF90" "#829375" "#626F52" "#475035" "#3B422D" "#2F3525"
 
 #### Tidy ####
 
-# First, need to correct an incorrect date.
+# First, need to correct a missing date.
 dat[1393, 4] <- ymd("2020-08-03")
 
 # Next, need to pivot to make into long format.
@@ -83,9 +84,9 @@ dat_long_trim <- dat_long %>%
   # make a year column to filter by
   mutate(year = year(Date)) %>%
   mutate(keep = case_when(watershed %in% c(5,6) & 
-                                year %in% c(2018, 2019, 2020, 2021, 2022, 2023) |
+                                year %in% c(2018, 2019, 2020, 2021, 2022, 2023, 2024) |
                               watershed == 1 &
-                                year %in% c(2018, 2019, 2020, 2021) |
+                                year %in% c(2018, 2019, 2020) |
                               watershed %in% c(2,9) &
                                 year %in% c(2018, 2019, 2020) |
                               watershed == 3 &
@@ -97,7 +98,7 @@ dat_long_trim <- dat_long %>%
   filter(keep == "Y")
 
 # Export data file for use in future scripts.
-# saveRDS(dat_long_trim, "data_working/aquatic_counts_long_051325.rds")
+# saveRDS(dat_long_trim, "data_working/aquatic_counts_long_070125.rds")
 
 #### Plot ####
 
@@ -170,9 +171,18 @@ dat_order_aq <- dat_order %>%
 #        height = 17,
 #        units = "cm")
 
-(fig2_w6 <- ggplot(dat_order_aq %>%
+dat_order_aq_wide <- dat_order_aq %>%
+  pivot_wider(names_from = Order, values_from = total_count) %>%
+  mutate(sum_total_count = dipteran + mayfly + stonefly + caddisfly) %>%
+  pivot_longer(cols = dipteran:sum_total_count,
+             names_to = "Order", values_to = "count") %>%
+  mutate(Order = factor(Order, levels = c("sum_total_count",
+                                        "dipteran", "caddisfly",
+                                        "stonefly", "mayfly")))
+
+(fig2_w6 <- ggplot(dat_order_aq_wide %>%
                      filter(watershed == 6), 
-                   aes(x = Date, y = total_count,
+                   aes(x = Date, y = count,
                        color = Order, group = year)) +
     geom_line(linewidth = 0.75) +
     scale_x_continuous(
@@ -181,19 +191,46 @@ dat_order_aq <- dat_order %>%
                         by = "2 year"),
       labels = ~ format(.x, "%Y")) +
     labs(y = "Count") +
-    scale_color_manual(values = c("#3900b3ff", "#714dbfff", "#9e6b90ff", "#cf9270ff")) +
+    scale_color_manual(values = c("black", "#3900b3ff", "#714dbfff", 
+                                  "#9e6b90ff", "#cf9270ff")) +
     facet_grid(Order ~ ., scales = "free_y",
-               labeller = labeller(Order = c('dipteran'="Diptera",
+               labeller = labeller(Order = c('sum_total_count'="Sum Total",
+                                             'dipteran'="Diptera",
                                              'mayfly'="Ephemeroptera",
                                              'stonefly'="Plecoptera", 
                                              'caddisfly'="Tricoptera"))) +
     theme_bw() +
-    theme(text = element_text(size = 16),
+    theme(text = element_text(size = 14),
           legend.position = "none"))
 
 # Export figure.
 # ggsave(plot = fig2_w6,
-#        filename = "figures/emerge_aq_w6_051424.jpg",
+#        filename = "figures/emerge_aq_w6_070125.jpg",
+#        width = 40,
+#        height = 20,
+#        units = "cm")
+
+# Create a new, overlapping version of this figure, while also displaying the sum total.
+(fig2_w6_v2 <- ggplot(dat_order_aq_wide %>%
+                     filter(watershed == 6), 
+                   aes(x = Date, group = year)) +
+    geom_line(aes(y = sum_total_count), color = "black", linewidth = 0.75) +
+    geom_line(aes(y = dipteran), color = "#3900b3ff", linewidth = 0.75) +
+    geom_line(aes(y = caddisfly), color = "#714dbfff", linewidth = 0.75) +
+    geom_line(aes(y = stonefly), color = "#9e6b90ff", linewidth = 0.75) +
+    geom_line(aes(y = mayfly), color = "#cf9270ff", linewidth = 0.75) +
+    scale_x_continuous(
+      breaks = seq.Date(as.Date("2018-01-01"), 
+                        as.Date("2024-12-31"), 
+                        by = "2 year"),
+      labels = ~ format(.x, "%Y")) +
+    labs(y = "Count") +
+    theme_bw() +
+    theme(text = element_text(size = 16)))
+
+# Export figure.
+# ggsave(plot = fig2_w6_v2,
+#        filename = "figures/emerge_aq_w6_070125.jpg",
 #        width = 40,
 #        height = 17,
 #        units = "cm")
@@ -231,6 +268,43 @@ dat_ws <- dat_long_trim %>%
 #        filename = "figures/emerge_ws_051324.jpg",
 #        width = 17,
 #        height = 30,
+#        units = "cm")
+
+# And create an overlapping version with things oriented to W6.
+dat_ws_wide <- dat_ws %>%
+  pivot_wider(names_from = watershed,
+              values_from = total_count)
+
+dat_ws <- dat_ws %>%
+  mutate(ws_y = paste(watershed, year, sep = "_")) %>%
+  mutate(ws_f = factor(watershed, levels = c("1", "2",
+                                             "3", "4", "5",
+                                             "9", "HBK", "6")))
+
+(fig3_v2 <- ggplot(dat_ws %>%
+                     filter(year < 2021), aes(x = Date, y = total_count,
+                            group = ws_y, color = ws_f, alpha = ws_f,
+                            linewidth = ws_f)) +
+    geom_line() +
+    scale_color_manual(values = c("#D3E3CA", "#C9DDBF", "#C0D7B5", 
+                                  "#AFC5A4", "#9BAF90", "#829375", 
+                                  "#626F52", "black")) +
+    scale_alpha_manual(values = c(1, 1, 1, 0.9, 0.9, 0.8, 0.8, 1)) +
+    scale_linewidth_manual(values = c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1)) +
+    scale_x_continuous(
+      breaks = seq.Date(as.Date("2018-01-01"), 
+                        as.Date("2020-12-31"), 
+                        by = "1 year"),
+      labels = ~ format(.x, "%Y")) +
+    labs(y = "Count") +
+    theme_bw() +
+    theme(text = element_text(size = 14), legend.position = "none"))
+
+# Export figure.
+# ggsave(plot = fig3_v2,
+#        filename = "figures/emerge_aq_3yr_070125.jpg",
+#        width = 20,
+#        height = 10,
 #        units = "cm")
 
 ##### Cumulative #####
