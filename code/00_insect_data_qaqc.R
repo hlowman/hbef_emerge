5329d286459efefb87499431952075b6ae97297e
Heili Lowman <heili.lowman@gmail.com>
2025-12-09 09:42:24 -0500

    Revised core dataset with counted insects through Dec 2025.

--- code/00_insect_data_qaqc.R
+++ code/00_insect_data_qaqc.R
@@ -8,7 +8,7 @@
 # of the HBEF aquatic insect emergence data.
 
 # Data was downloaded from https://hbwater.org/restricted_QAQC/
-# on August 14, 2025.
+# on December 5, 2025.
 
 #### Setup ####
 
@@ -18,12 +18,43 @@ library(tidyverse)
 library(lubridate)
 
 # Load data.
-dat <- read_csv("data_raw/sticky_trap_counts_081425.csv")
+dat <- read_csv("data_raw/sticky_trap_counts_120525.csv")
 
 #### Tidy ####
 
-# First, need to correct a missing date.
-dat[1393, 4] <- ymd("2020-08-03")
+# First, need to correct missing dates.
+dat <- dat %>%
+  mutate(date_ed = case_when(sample_id %in% c("ST231101",
+                                              "ST231102",
+                                              "ST231103",
+                                              "ST231104",
+                                              "ST231105") ~ ymd("2023-10-23"),
+                             sample_id %in% c("ST231141",
+                                              "ST231142") ~ ymd("2023-10-30"),
+                             sample_id %in% c("ST200456",
+                                              "ST200457",
+                                              "ST200458",
+                                              "ST200459",
+                                              "ST200460") ~ ymd("2020-06-01"),
+                             sample_id %in% c("ST200496",
+                                              "ST200497",
+                                              "ST200498",
+                                              "ST200499",
+                                              "ST200500") ~ ymd("2020-06-08"),
+                             sample_id %in% c("ST200536",
+                                              "ST200537",
+                                              "ST200538",
+                                              "ST200539",
+                                              "ST200540") ~ ymd("2020-06-15"),
+                             sample_id %in% c("ST200576",
+                                              "ST200577",
+                                              "ST200578",
+                                              "ST200579",
+                                              "ST200580") ~ ymd("2020-06-22"),
+                             sample_id %in% c("ST211370") ~ ymd("2021-11-08"),
+                             sample_id %in% c("ST210312") ~ ymd("2021-05-03"),
+                             sample_id %in% c("ST200808") ~ ymd("2020-08-03"),
+                             TRUE ~ mdy(date)))
 
 # Next, need to pivot to make into long format.
 dat_long <- dat %>%
@@ -34,7 +65,7 @@ dat_long <- dat %>%
 # Also need to add additional columns to plot more easily.
 dat_long <- dat_long %>%
   # create formatted date column
-  mutate(Date = ymd(date)) %>%
+  mutate(Date = ymd(date_ed)) %>%
   # create separate order column
   mutate(Order = factor(case_when(id %in% c("dipteran_large",
                                             "dipteran_small") ~ "dipteran",
@@ -67,14 +98,15 @@ dat_long <- dat_long %>%
                                     "mayfly_small",
                                     "stonefly_small",
                                     "other_small") ~ "small")) %>%
-  mutate(Year = year(date))
+  mutate(Year = year(Date))
 
 # Examine data coverage.
-ggplot(dat_long, aes(x = date,
+ggplot(dat_long, aes(x = Date,
                      y = watershed,
                      color = watershed,
                      group = Year)) +
   geom_line() +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
   facet_grid(watershed ~ ., scales = "free_y") +
   theme_bw() +
   theme(legend.position = "none")
@@ -85,9 +117,7 @@ dat_long_trim <- dat_long %>%
   mutate(year = year(Date)) %>%
   mutate(keep = case_when(watershed %in% c("5","6") & 
                             year %in% c(2018, 2019, 2020, 2021, 2022, 2023, 2024) |
-                            watershed %in% c("1","3","4") &
-                            year %in% c(2018, 2019, 2020, 2021) |
-                            watershed %in% c("2","9") &
+                            watershed %in% c("1","2", "3","4", "9") &
                             year %in% c(2018, 2019, 2020) |
                             watershed %in% c("HBK") &
                             year %in% c(2018, 2019) ~ "Y",
@@ -100,6 +130,6 @@ dat_long_aq <- dat_long_trim %>%
   filter(Order %in% c("dipteran", "caddisfly", "stonefly", "mayfly"))
 
 # Export data file for use in future scripts.
-saveRDS(dat_long_aq, "data_working/aquatic_counts_complete_yrs_081425.rds")
+saveRDS(dat_long_aq, "data_working/aquatic_counts_complete_yrs_120925.rds")
 
 # End of script.
